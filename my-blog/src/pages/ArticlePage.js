a1d906ea27689023d01770fe82ac3710558bd19d
Kenny Su <kfernandosuqiu@gmail.com>
2023-02-17 10:02:26 -0500

    4. Connecting the Front and Back End

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import axios from "axios";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data.article;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, []);
+
   const article = articles.find((article) => article.name === articleId);
+
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
 
   if (!article) {
     return <NotFoundPage />;
@@ -13,9 +34,18 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s).</p>
+      </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
+      <CommentsList comments={articleInfo.comments} />
     </>
   );
 };
